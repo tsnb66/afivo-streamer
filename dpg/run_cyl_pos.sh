d86da648e58067de9c67c90527c079d464c0acc2
Jannis Teunissen <jannis@teunissen.net>
2017-03-10 15:40:14 +0100

    run scripts

---
+++ dpg/run_cyl_pos.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+./streamer_2d dpg/cyl_pos.cfg dpg/By.cfg dpg/N2_0T.cfg
+./streamer_2d dpg/cyl_pos.cfg dpg/By.cfg dpg/N2_10T.cfg
+./streamer_2d dpg/cyl_pos.cfg dpg/By.cfg dpg/N2_20T.cfg
