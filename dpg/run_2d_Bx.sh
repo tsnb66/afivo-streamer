9b5497bfce9d3e822a0c627173d27c5eb53ef509
Jannis Teunissen <jannis@teunissen.net>
2017-03-10 14:40:31 +0100

    New configs

---
+++ dpg/run_2d_Bx.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+./streamer_2d dpg/2d.cfg dpg/Bx.cfg dpg/N2_0T.cfg
+./streamer_2d dpg/2d.cfg dpg/Bx.cfg dpg/N2_10T.cfg
+./streamer_2d dpg/2d.cfg dpg/Bx.cfg dpg/N2_20T.cfg
+./streamer_2d dpg/2d.cfg dpg/Bx.cfg dpg/N2_30T.cfg
+./streamer_2d dpg/2d.cfg dpg/Bx.cfg dpg/N2_40T.cfg
---
+++ dpg/run_swarm_2d.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+./streamer_2d dpg/swarm.cfg dpg/Bz.cfg dpg/N2_0T.cfg
+./streamer_2d dpg/swarm.cfg dpg/Bz.cfg dpg/N2_10T.cfg
+./streamer_2d dpg/swarm.cfg dpg/Bz.cfg dpg/N2_20T.cfg
+./streamer_2d dpg/swarm.cfg dpg/Bz.cfg dpg/N2_30T.cfg
+./streamer_2d dpg/swarm.cfg dpg/Bz.cfg dpg/N2_40T.cfg
+
+./streamer_2d dpg/swarm.cfg dpg/Bx.cfg dpg/N2_0T.cfg
+./streamer_2d dpg/swarm.cfg dpg/Bx.cfg dpg/N2_10T.cfg
+./streamer_2d dpg/swarm.cfg dpg/Bx.cfg dpg/N2_20T.cfg
+./streamer_2d dpg/swarm.cfg dpg/Bx.cfg dpg/N2_30T.cfg
+./streamer_2d dpg/swarm.cfg dpg/Bx.cfg dpg/N2_40T.cfg
+
+./streamer_2d dpg/swarm.cfg dpg/By.cfg dpg/N2_0T.cfg
+./streamer_2d dpg/swarm.cfg dpg/By.cfg dpg/N2_10T.cfg
+./streamer_2d dpg/swarm.cfg dpg/By.cfg dpg/N2_20T.cfg
+./streamer_2d dpg/swarm.cfg dpg/By.cfg dpg/N2_30T.cfg
+./streamer_2d dpg/swarm.cfg dpg/By.cfg dpg/N2_40T.cfg
